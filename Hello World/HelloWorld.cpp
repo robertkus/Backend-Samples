a263b99c539152b7e1bb401435dce125455218fc
Robert Kus <68250500+robertkus@users.noreply.github.com>
2023-08-19 22:45:19 -0400

    Add files via upload
    Beginner's C++ Hello World project (displays Hello World)

---
+++ Hello World/HelloWorld.cpp
@@ -0,0 +1,29 @@
+//HelloWorld.ccp -- This project displays the message "Hello World!" and introduces the very basic elements of a C++ Program
+//CSIS 111- D01
+//<Sources if necessary>
+
+//Include statements
+
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+//Global declarations: Constants and type definitions only -- no variables
+
+//Function
+int main()
+{
+	//In cout statement below SUBSTITUTE your name and lab number
+	cout << "Robert Kus -- Lab 1" << endl << endl;
+
+	//Variable declarations
+
+	//Program logic
+	cout << "Hello World!" << endl;
+
+	//Closing program statements 
+	system("pause");
+	return 0;
+}
+//Function definitions
