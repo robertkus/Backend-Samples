e1291067f06ded8644e8245e50313db623ddf324
Robert Kus <68250500+robertkus@users.noreply.github.com>
2024-11-24 23:22:19 -0500

    Add files via upload

---
+++ rk_classes_and_objects_programming/frmAssignment6.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAssignment6
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstMonthIncome = New System.Windows.Forms.ListBox()
+        Me.lstMonthExpense = New System.Windows.Forms.ListBox()
+        Me.btnEnterIncome = New System.Windows.Forms.Button()
+        Me.btnEnterExpense = New System.Windows.Forms.Button()
+        Me.lblAmount = New System.Windows.Forms.Label()
+        Me.lblTotalIncome = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.txtTotalIncome = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lstMonthIncome
+        '
+        Me.lstMonthIncome.FormattingEnabled = True
+        Me.lstMonthIncome.Items.AddRange(New Object() {"Current month income", "-----------------------------------------", ""})
+        Me.lstMonthIncome.Location = New System.Drawing.Point(40, 54)
+        Me.lstMonthIncome.Name = "lstMonthIncome"
+        Me.lstMonthIncome.Size = New System.Drawing.Size(230, 329)
+        Me.lstMonthIncome.TabIndex = 3
+        Me.lstMonthIncome.TabStop = False
+        '
+        'lstMonthExpense
+        '
+        Me.lstMonthExpense.FormattingEnabled = True
+        Me.lstMonthExpense.Items.AddRange(New Object() {"Current month expenses", "-----------------------------------------", ""})
+        Me.lstMonthExpense.Location = New System.Drawing.Point(495, 54)
+        Me.lstMonthExpense.Name = "lstMonthExpense"
+        Me.lstMonthExpense.Size = New System.Drawing.Size(230, 329)
+        Me.lstMonthExpense.TabIndex = 4
+        Me.lstMonthExpense.TabStop = False
+        '
+        'btnEnterIncome
+        '
+        Me.btnEnterIncome.Location = New System.Drawing.Point(329, 159)
+        Me.btnEnterIncome.Name = "btnEnterIncome"
+        Me.btnEnterIncome.Size = New System.Drawing.Size(105, 44)
+        Me.btnEnterIncome.TabIndex = 1
+        Me.btnEnterIncome.Text = "Enter &Income"
+        Me.btnEnterIncome.UseVisualStyleBackColor = True
+        '
+        'btnEnterExpense
+        '
+        Me.btnEnterExpense.Location = New System.Drawing.Point(329, 232)
+        Me.btnEnterExpense.Name = "btnEnterExpense"
+        Me.btnEnterExpense.Size = New System.Drawing.Size(105, 44)
+        Me.btnEnterExpense.TabIndex = 2
+        Me.btnEnterExpense.Text = "Enter &Expense"
+        Me.btnEnterExpense.UseVisualStyleBackColor = True
+        '
+        'lblAmount
+        '
+        Me.lblAmount.AutoSize = True
+        Me.lblAmount.Location = New System.Drawing.Point(363, 54)
+        Me.lblAmount.Name = "lblAmount"
+        Me.lblAmount.Size = New System.Drawing.Size(43, 13)
+        Me.lblAmount.TabIndex = 4
+        Me.lblAmount.Text = "Amount"
+        '
+        'lblTotalIncome
+        '
+        Me.lblTotalIncome.AutoSize = True
+        Me.lblTotalIncome.Location = New System.Drawing.Point(351, 359)
+        Me.lblTotalIncome.Name = "lblTotalIncome"
+        Me.lblTotalIncome.Size = New System.Drawing.Size(69, 13)
+        Me.lblTotalIncome.TabIndex = 5
+        Me.lblTotalIncome.Text = "Total Income"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(334, 81)
+        Me.txtAmount.Multiline = True
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(100, 56)
+        Me.txtAmount.TabIndex = 1
+        Me.txtAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtTotalIncome
+        '
+        Me.txtTotalIncome.Location = New System.Drawing.Point(334, 384)
+        Me.txtTotalIncome.Multiline = True
+        Me.txtTotalIncome.Name = "txtTotalIncome"
+        Me.txtTotalIncome.ReadOnly = True
+        Me.txtTotalIncome.Size = New System.Drawing.Size(100, 54)
+        Me.txtTotalIncome.TabIndex = 5
+        Me.txtTotalIncome.TabStop = False
+        Me.txtTotalIncome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'frmAssignment6
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.rk_classes_and_objects_programming.My.Resources.Resources.imgFinances
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(801, 518)
+        Me.Controls.Add(Me.txtTotalIncome)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.lblTotalIncome)
+        Me.Controls.Add(Me.lblAmount)
+        Me.Controls.Add(Me.btnEnterExpense)
+        Me.Controls.Add(Me.btnEnterIncome)
+        Me.Controls.Add(Me.lstMonthExpense)
+        Me.Controls.Add(Me.lstMonthIncome)
+        Me.Name = "frmAssignment6"
+        Me.Text = "Assignment 6"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstMonthIncome As ListBox
+    Friend WithEvents lstMonthExpense As ListBox
+    Friend WithEvents btnEnterIncome As Button
+    Friend WithEvents btnEnterExpense As Button
+    Friend WithEvents lblAmount As Label
+    Friend WithEvents lblTotalIncome As Label
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents txtTotalIncome As TextBox
+End Class
---
+++ rk_classes_and_objects_programming/frmAssignment6.vb
@@ -0,0 +1,171 @@
+﻿'CLASSES AND OBJECTS PROGRAMMING ASSIGNMENT
+'This assignment utilizes properties and methods from a class to store and manipulate user inputs (in this case, the balance).
+'The main class will create an instance of the external class, allowing it to store user inputs to it properties,
+'and utilize its methods to perform operations on those properties.
+'Robert Kus
+'I will not use code that was never covered in class or in our textbook.
+'If you do you must be able to explain your code when asked by the professor.
+'Using code outside of the resources provided,
+'it can be flagged and reported as an academic integrity violation if there is any suspicion of copying/cheating.
+
+Public Class frmAssignment6
+
+    'Declare global variables
+    Dim enteredAmount As Double
+    Dim budgetReceipts As New BudgetReceipts
+    Dim amountDescription As String
+
+    'Get the user's initial budget when the application first opens.
+    'If no value or an invalid amount is entered, continue to prompt the user until one is entered.
+    Private Sub frmAssignment6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim isValidBudget = False
+        Dim initialBalance As String
+
+        'Initialize enteredamount
+        enteredAmount = 0
+
+        While isValidBudget = False
+            initialBalance = InputBox("Please enter a starting balance in the input box provided (ex: 400.00).", "Enter Starting Balance")
+            If initialBalance = "" Then
+                MessageBox.Show("Please enter a value before continuing.")
+                isValidBudget = False
+            Else
+                Double.TryParse(initialBalance, enteredAmount)
+                If enteredAmount <= 0 Then
+                    MessageBox.Show("Please enter an integer or decimal value grater than zero.")
+                    enteredAmount = 0
+                    isValidBudget = False
+                Else
+                    isValidBudget = True
+                End If
+            End If
+        End While
+
+        'Add the entered amount to the Balance.
+        budgetReceipts.Income(enteredAmount)
+
+        'Add the entered amount as a starting balance to the income list
+        lstMonthIncome.Items.Add("Starting Balance - " & enteredAmount.ToString("C2"))
+
+        'Update the Total Income based on the Balance value.
+        txtTotalIncome.Text = budgetReceipts.Balance.ToString("C2")
+
+        'Reset the enteredAmount and amountDescription
+        ResetAmountAndDescription()
+    End Sub
+
+    'Allow the user to add a source of income to the balance (value and source)
+    Private Sub btnEnterIncome_Click(sender As Object, e As EventArgs) Handles btnEnterIncome.Click
+        'Check if an Amount was entered and if the value is in the correct format.
+        Dim isValidInput = CheckForValidInput()
+
+        'Immediately exit the code if there is any invalid inputs
+        If isValidInput = False Then
+            Return
+        End If
+
+        'Get the decription of the income from the user. Exit out of Sub if no description is entered.
+        amountDescription = InputBox("Please provide a description of the amount.", "Amount description")
+
+        If amountDescription = "" Then
+            MessageBox.Show("No description was entered. Unable to add income.")
+            Return
+        End If
+
+        'Add the entered amount to the Balance.
+        budgetReceipts.Income(enteredAmount)
+
+        'Add the new value to the income list.
+        lstMonthIncome.Items.Add(amountDescription & " - " & enteredAmount.ToString("C2"))
+
+        'Update the Total Income based on the Balance value.
+        txtTotalIncome.Text = budgetReceipts.Balance.ToString("C2")
+
+        'Reset the enteredAmount and amountDescription
+        ResetAmountAndDescription()
+    End Sub
+
+    Private Sub btnEnterExpense_Click(sender As Object, e As EventArgs) Handles btnEnterExpense.Click
+        'Check if an Amount was entered and if the value is in the correct format.
+        Dim isValidInput = CheckForValidInput()
+
+        'Immediately exit the code if there is any invalid inputs
+        If isValidInput = False Then
+            Return
+        End If
+
+        'Get the decription of the expense from the user. Exit out of Sub if no description is entered.
+        amountDescription = InputBox("Please provide a description of the amount.", "Amount description")
+
+        If amountDescription = "" Then
+            MessageBox.Show("No description was entered. Unable to add expense.")
+            Return
+        End If
+
+        'Subtract the entered amount from the Balance.
+        budgetReceipts.Expenses(enteredAmount)
+
+        'Add the new value to the expense list.
+        lstMonthExpense.Items.Add(amountDescription & " - " & enteredAmount.ToString("C2"))
+
+        'Update the Total Income based on the Balance value.
+        txtTotalIncome.Text = budgetReceipts.Balance.ToString("C2")
+
+        'Display a warning message while the budget drops below zero.
+        If budgetReceipts.Balance < 0 Then
+            MessageBox.Show("Warning: You currently hold a negative balance.")
+        End If
+
+        'Reset the enteredAmount and amountDescription
+        ResetAmountAndDescription()
+    End Sub
+
+    'Reset the enteredAmount and amountDescription variables to their initial values.
+    Private Sub ResetAmountAndDescription()
+        enteredAmount = 0
+        amountDescription = ""
+    End Sub
+
+    'Check if a user's input is valid.
+    'Display the appropriate error message and return false if the input is invalid.
+    'Otherwise, return true.
+    Private Function CheckForValidInput() As Boolean
+        If Me.txtAmount.Text = "" Then
+            MessageBox.Show("No amount was entered. Please enter a numeric value greater than zero in the Amount box.")
+            Return False
+        End If
+
+        Double.TryParse(Me.txtAmount.Text, enteredAmount)
+
+        If enteredAmount <= 0 Then
+            MessageBox.Show("The value entered is invalid. Please enter a numeric value greater than zero in the Amount box.")
+            enteredAmount = 0
+            Return False
+        End If
+
+        Return True
+    End Function
+End Class
+
+'This an external class for holding and handling budget information.
+Public Class BudgetReceipts
+    'Declare the private currentBalance property
+    Private currentBalance As Double
+
+    'Set the property balance so that the current balance can only be read publicly
+    Public ReadOnly Property Balance() As Double
+        Get
+            Return currentBalance
+        End Get
+    End Property
+
+    'Add the entered income to the Balance
+    Public Sub Income(incomeSource As Double)
+        currentBalance += incomeSource
+    End Sub
+
+    'Subtract the entered expense from the Balance
+    Public Sub Expenses(expenseSource As Double)
+        currentBalance -= expenseSource
+    End Sub
+End Class
